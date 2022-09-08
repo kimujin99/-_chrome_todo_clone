77667d553be15ae2f0574a81f38193a4c7659984
KUJ <87636036+kimujin99@users.noreply.github.com>
2022-09-08 17:52:10 +0900

    todo.js 업데이트
    주석 추가 
    로컬 스토리지 유지, 수정, 삭제 로직 추가

--- MOMENTUM/js/todo.js
+++ MOMENTUM/js/todo.js
@@ -2,34 +2,73 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+//local storage에 저장할 array
+//let : 기존 local storage에 있는 array를 업데이트하기 위해
+let todos = [];
+
+function saveTodos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
+//submit 이벤트
 function onTodoSubmit(event) {
     event.preventDefault(); //기본동작 막기
     const newTodo = todoInput.value;
     todoInput.value = ""; //입력창 클리어
 
-    paintTodo(newTodo);
+    //todos obj 만들기
+    const newTodoObj = {
+        text : newTodo,
+        id : Date.now(),
+    }
+
+    //array에 저장
+    todos.push(newTodoObj);
+    //화면에 출력
+    paintTodo(newTodoObj);
+    //local storage에 저장
+    saveTodos();
 };
 
 function deleteTodo(event) {
+    //어떤 리스트가 선택되었는지 판단
     const li = event.target.parentElement;
+    todos = todos.filter((todo) => todo.id !== parseInt(li.id));
     li.remove();
+
+    saveTodos();
 }
 
 //리스트 그리기
 function paintTodo(newTodo) {
+    //<li>만들기
     const li = document.createElement("li");
-
+    li.id = newTodo.id;
+    //<span>만들기
     const span = document.createElement("span");
-    span.innerText = newTodo;
-
+    span.innerText = newTodo.text;
+    //<button>만들기
     const button = document.createElement("button");
     button.innerText = "❌";
+    //버튼 클릭 이벤트 - 삭제
     button.addEventListener("click", deleteTodo);
 
+    //<li>안에 추가
     li.appendChild(span);
     li.appendChild(button);
-    
+    //<ul>안에 추가
     todoList.appendChild(li);
 }
 
 todoForm.addEventListener("submit", onTodoSubmit);
+
+//local storage의 todos를 가져와 세팅
+const savedTodos = localStorage.getItem(TODOS_KEY);
+if(savedTodos !== null){
+    const parsedTodos = JSON.parse(savedTodos);
+    //local storage의 todos를 array에 업데이트
+    todos = parsedTodos;
+    parsedTodos.forEach(paintTodo);
+}
